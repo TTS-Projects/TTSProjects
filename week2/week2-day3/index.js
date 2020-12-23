707f692ed028dc722d0e51f3b44a1325c03e8128
Miguel <Garzarobm@utexas.edu>
2020-12-23 09:33:54 -0600

    first commit

---
+++ week2/week2-day3/index.js
@@ -0,0 +1,17 @@
+console.log('hello world');
+//Challenge 1
+let person_cups = 2
+if (person_cups < 3) {
+    console.log("Yes I'll take another cup of coffee.")
+}
+else if (person_cups > 3) {
+    console.log("I think I'm okay for now.")
+}
+//Challenge 2
+let miles = 5000
+if (miles > 500) {
+    console.log("Yes I think I need an oil change.")
+}
+else {
+    console.log("My car doesn't need an oil change yet.")
+}
